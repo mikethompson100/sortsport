1c925e42b6195e912d00ed9284b36e8fb71f6ab2
mikethompson100 <mikethompson100@hotmail.com>
2025-01-27 08:41:15 -0500

    Added more columns to table.

--- src/components/TeamsTable.js
+++ src/components/TeamsTable.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 function TeamsTable(props) {
     const [sortedBy, setSortedBy] = useState("NAME"); // State to store fetched data
-    if (props.statistics.loading){
+    if (props.statistics.loading) {
         return <div>Loading...</div>;
     }
 
@@ -26,9 +26,19 @@ function TeamsTable(props) {
         else if (sortedBy === "-GO") return b.stat.groundOuts - a.stat.groundOuts;
         else if (sortedBy === "AO") return a.stat.airOuts - b.stat.airOuts;
         else if (sortedBy === "-AO") return b.stat.airOuts - a.stat.airOuts;
+        else if (sortedBy === "BB") return a.stat.baseOnBalls - b.stat.baseOnBalls;
+        else if (sortedBy === "-BB") return b.stat.baseOnBalls - a.stat.baseOnBalls;
+        else if (sortedBy === "AVG") return a.stat.avg - b.stat.avg;
+        else if (sortedBy === "-AVG") return b.stat.avg - a.stat.avg;
+        else if (sortedBy === "GIDP") return a.stat.groundIntoDoublePlay - b.stat.groundIntoDoublePlay;
+        else if (sortedBy === "-GIDP") return b.stat.groundIntoDoublePlay - a.stat.groundIntoDoublePlay;
+        else if (sortedBy === "RBI") return a.stat.rbi - b.stat.rbi;
+        else if (sortedBy === "-RBI") return b.stat.rbi - a.stat.rbi;
+        else if (sortedBy === "HIT") return a.stat.rbi - b.stat.rbi;
+        else if (sortedBy === "-HIT") return b.stat.rbi - a.stat.rbi;
         return 0; // Default case if no sorting is applied
     });
-    
+
     // Render the component
     return (
         <div>
@@ -39,23 +49,33 @@ function TeamsTable(props) {
                     <tbody>
                         <tr>
                             <td>Season:</td>
-                            <td><button onClick={() => handleSort('NAME')}>Team:</button></td>
-                            <td><button onClick={() => handleSort('HR')}>HRs:</button></td>
-                            <td><button onClick={() => handleSort('ABPH')}>AB/PH:</button></td>
-                            <td><button onClick={() => handleSort('SO')}>SOs:</button></td>
-                            <td><button onClick={() => handleSort('GO')}>GOs:</button></td>
-                            <td><button onClick={() => handleSort('AO')}>AOs:</button></td>
+                            <td class="left">Team:</td>
+                            <td><button onClick={() => handleSort('AVG')} title="Batting Average">AVG:</button></td>
+                            <td><button onClick={() => handleSort('HIT')} title="Hits">Hits:</button></td>
+                            <td><button onClick={() => handleSort('HR')} title="Homeruns">HRs:</button></td>
+                            <td><button onClick={() => handleSort('RBI')} title="Homeruns">RBIs:</button></td>
+                            <td><button onClick={() => handleSort('ABPH')} title="At Bats per Homerun">AB/PHr:</button></td>
+                            <td><button onClick={() => handleSort('BB')} title="Walks">Walks:</button></td>
+                            <td><button onClick={() => handleSort('SO')} title="Strikeouts">SOs:</button></td>
+                            <td><button onClick={() => handleSort('GO')} title="Groundouts">GOs:</button></td>
+                            <td><button onClick={() => handleSort('AO')} title="Airouts">AOs:</button></td>
+                            <td><button onClick={() => handleSort('GIDP')} title="Ground into Double Play">GIDP:</button></td>
                         </tr>
                         {sortedRecords.map(record => (
-                        <tr key={record.team.id}>
-                            <td>{record.season}</td>
-                            <td>{record.team.name}</td>
-                            <td>{record.stat.homeRuns}</td>
-                            <td>{record.stat.atBatsPerHomeRun}</td>
-                            <td>{record.stat.strikeOuts}</td>
-                            <td>{record.stat.groundOuts}</td>
-                            <td>{record.stat.airOuts}</td>
-                        </tr>
+                            <tr key={record.team.id}>
+                                <td>{record.season}</td>
+                                <td class="left">{record.team.name}</td>
+                                <td>{record.stat.avg}</td>
+                                <td>{record.stat.hits}</td>
+                                <td>{record.stat.homeRuns}</td>
+                                <td>{record.stat.rbi}</td>
+                                <td>{record.stat.atBatsPerHomeRun}</td>
+                                <td>{record.stat.baseOnBalls}</td>
+                                <td>{record.stat.strikeOuts}</td>
+                                <td>{record.stat.groundOuts}</td>
+                                <td>{record.stat.airOuts}</td>
+                                <td>{record.stat.groundIntoDoublePlay}</td>
+                            </tr>
                         ))}
                     </tbody>
                 </table>
--- src/hooks/useData.js
+++ src/hooks/useData.js
@@ -6,23 +6,22 @@ function useData() {
     const [loading, setLoading] = useState(true); // State to manage loading
     const [error, setError] = useState(null); // State to store errors
 
-const fetchData = async () => {
-    try {
-        const url = `${MLB_STATS}`;
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
+    const fetchData = async () => {
+        try {
+            const url = `${MLB_STATS}`;
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            let data = await response.json();
+            const targetData = data.stats[0].splits;
+            setData(targetData); // Update state with fetched data
+        } catch (err) {
+            setError(err.message); // Update state with error message
+        } finally {
+            setLoading(false); // Set loading to false once done
         }
-        let data = await response.json();
-        const targetData = data.stats[0].splits;
-        
-        setData(targetData); // Update state with fetched data
-    } catch (err) {
-        setError(err.message); // Update state with error message
-    } finally {
-        setLoading(false); // Set loading to false once done
-    }
-};
+    };
 
     // Use useEffect to call fetchData on component mount
     useEffect(() => {
